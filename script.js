2387c1b2c6a8976aded2bf08205ac5a1a5dba88c
TahmidKwt <100180424+TahmidKwt@users.noreply.github.com>
2023-09-02 13:18:02 +0600

    modify html and js file

--- script.js
+++ script.js
@@ -1,52 +1,72 @@
-const rock = document.querySelector(".rock-btn");
-const paper = document.querySelector(".paper-btn");
-const scissors = document.querySelector(".scissors-btn");
-const result = document.querySelector(".round-result");
-const winner = document.querySelector(".game-result");
-const pscore = document.querySelector(".player-score");
-const cscore = document.querySelector(".computer-score");
-const reset = document.querySelector(".reset-game");
+let rock = document.querySelector(".rock-btn");
+let paper = document.querySelector(".paper-btn");
+let scissors = document.querySelector(".scissors-btn");
+let result = document.querySelector(".round-result");
+let winner = document.querySelector(".game-result");
+let pscore = document.querySelector(".player-score");
+let cscore = document.querySelector(".computer-score");
+let reset = document.querySelector(".reset-btn");
+let rule = document.getElementById("game-rule");
 
 rock.addEventListener("click", () => playRound("ROCK"));
 paper.addEventListener("click", () => playRound("PAPER"));
 scissors.addEventListener("click", () => playRound("SCISSORS"));
-reset.addEventListener("click", () => resetGame);
 
 function getComputerChoice() {
-  const choices = ["ROCK", "PAPER", "SCISSORS"];
-  const randomize = Math.floor(Math.random() * choices.length);
 
-  return choices[randomize];
+	const choices = ["ROCK", "PAPER", "SCISSORS"];
+	const randomize = Math.floor(Math.random() * choices.length);
+	return choices[randomize];
+
 }
 
 let playerScore = 0;
 let computerScore = 0;
 
 function playRound(playerSelection) {
-  let computerSelection = getComputerChoice();
-
-  if (playerSelection === computerSelection) {
-    
-    result.innerHTML = "Result: It's a tie.";
-    
-  } else if ((playerSelection === "ROCK" && computerSelection === "PAPER") || (playerSelection === "PAPER" && computerSelection === "SCISSORS") || (playerSelection === "SCISSORS" && computerSelection === "ROCK")) {
-    
-    result.textContent = `Result: You lose! ${computerSelection} beats ${playerSelection}`;
-    computerScore++;
-    cscore.textContent = `Computer Score: ${computerScore}`;
   
-  } else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") || (playerSelection === "PAPER" && computerSelection === "ROCK") || (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
+	let computerSelection = getComputerChoice();
+  
+	if (playerSelection === computerSelection) {
     
-    result.textContent = `Result: You win! ${playerSelection} beats ${computerSelection}`;
-    playerScore++;
-    pscore.textContent = `Player Score: ${playerScore}`;
+		result.innerHTML = "Result: It's a tie.";
     
-  }
-
-  if (computerScore === 5) {
-    winner.textContent = "Game Over! Computer is the Winner!";
-  } else if (playerScore === 5) {
-    winner.textContent = "Game Over! You are the Winner!";
-  }
+	} else if ((playerSelection === "ROCK" && computerSelection === "PAPER") || (playerSelection === "PAPER" && computerSelection === "SCISSORS") || (playerSelection === "SCISSORS" && computerSelection === "ROCK")) {
+    
+		result.textContent = `Result: You lose! ${computerSelection} beats ${playerSelection}`;
+		computerScore++;
+		cscore.textContent = `Computer Score: ${computerScore}`;
+  
+	} else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") || (playerSelection === "PAPER" && computerSelection === "ROCK") || (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
+    
+		result.textContent = `Result: You win! ${playerSelection} beats ${computerSelection}`;
+		playerScore++;
+		pscore.textContent = `Player Score: ${playerScore}`;
+  
+	}
+  
+	if ((computerScore === 5) || (playerScore === 5)) {
+	  
+		gameEnd();
+	}
 }
 
+function gameEnd() {
+
+	rule.style.display = "none";
+	result.style.display = "hiddden";
+
+        if (computerScore === 5) {
+		winner.textContent = "Game Over! Computer is the Winner!" 
+	} else if (playerScore === 5) {
+		winner.textContent = "Game Over! You are the Winner!";
+	}
+
+	playerScore = 0;
+	computerScore = 0;
+
+	pscore.textContent = `Player Score: ${playerScore}`;
+	cscore.textContent = `Computer Score: ${computerScore}`;
+}
+   
+reset.addEventListener("click", gameEnd);
